711b7b939cffead60add83fe5347799d677a1746
Robert Shields <robert.shieldsiv@gmail.com>
2020-03-12 01:18:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,20 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/' do
-    "Hello World"
-  end
+  # get '/' do
+  #   "Hello World"
+  # end
   
-  get '/' do
-    "Hello World"
-  end
+  # get '/' do
+  #   "Hello World"
+  # end
   
-  get '/' do
-    "Hello World"
-  end
+  # get '/' do
+  #   "Hello World"
+  # end
   
-  get '/' do
-    "Hello World"
-  end
+  # get '/' do
+  #   "Hello World"
+  # end
   
 end
